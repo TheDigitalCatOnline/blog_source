30801f992dd8e9a52e4fe54a4a5b351676700a24
Leonardo Giordani <giordani.leonardo@gmail.com>
2016-02-29 18:55:48 +0100

    Fixed online address

--- pelican/publishconf.py
+++ pelican/publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://lgiordani.com'
+SITEURL = 'http://blog.thedigitalcatonline.com'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'atom.xml'
