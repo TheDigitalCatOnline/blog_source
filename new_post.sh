64281441671b07f31949a9d458b774aa78196475
Leonardo Giordani <giordani.leonardo@gmail.com>
2017-07-12 09:04:53 +0100

    Fixed the extraction of categories from previous posts

--- new_post.sh
+++ new_post.sh
@@ -11,10 +11,11 @@ touch ${article_file}
 
 echo "Title: " > ${article_file}
 echo "Date: $(date +"%Y-%m-%d %H:00:00 +0100")" >> ${article_file}
-echo "Category: " >> ${article_file}
+echo "Category: $(grep -h "Category:" *.markdown | sed -r -e s,"Category: ",, -e s/", "/","/g | xargs -d "," -n1 | grep -Ev "^$" | sort | uniq | paste -d"," -s | sed s/","/", "/g
+)" >> ${article_file}" >> ${article_file}
 echo "Tags: $(grep -h Tags *.markdown | sed -r -e s,"Tags: ",, -e s/", "/","/g | xargs -d "," -n1 | grep -Ev "^$" | sort | uniq | paste -d"," -s | sed s/","/", "/g
 )" >> ${article_file}
-echo "Authors: The Digital Cat Online" >> ${article_file}
+echo "Authors: Leonardo Giordani" >> ${article_file}
 echo "Slug: ${article_slug}" >> ${article_file}
 echo "Summary: " >> ${article_file}
 
